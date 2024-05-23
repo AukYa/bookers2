e7311a5cf6e66e66d2573c732f0c40fc22dfe3cb
AukYa <162345538+AukYa@users.noreply.github.com>
2024-05-23 10:02:32 +0000

    [Add]index,showのviewページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,15 +7,16 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(:id)
   end
 
   def index
     @books = Book.all
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
