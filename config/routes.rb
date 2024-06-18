376da0ebaf1665d747b77aa961a4a7421b147ce5
AukYa <162345538+AukYa@users.noreply.github.com>
2024-06-18 10:23:20 +0000

    [Add]検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update]
   get 'home/about', to: 'homes#about', as: :about
   root to: 'homes#top'
+  get 'search', to: 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
